bfe24c4f2e7d26a86e86b371f71d50e135fa04cf
olawanlejoel <info4tobest2@gmail.com>
2022-06-27 11:12:52 +0100

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,14 +10,16 @@ import CategoryPage from './pages/CategoryPage';
 const App = () => {
 	return (
 		<div>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/shop" element={<Shop />} />
-				<Route path="/shop/:category" element={<CategoryPage />} />
-				<Route path="/shop/:category/:slug" element={<SingleProduct />} />
-			</Routes>
-			<Footer />
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+					<Route path="/shop/:category" element={<CategoryPage />} />
+					<Route path="/shop/:category/:slug" element={<SingleProduct />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
-);
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
