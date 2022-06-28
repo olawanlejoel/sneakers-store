22bd33eec22535b5d7152101099db22f1d96539d
olawanlejoel <info4tobest2@gmail.com>
2022-06-28 14:26:46 +0100

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,16 +10,14 @@ import CategoryPage from './pages/CategoryPage';
 const App = () => {
 	return (
 		<div>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/shop" element={<Shop />} />
-					<Route path="/shop/:category" element={<CategoryPage />} />
-					<Route path="/shop/:category/:slug" element={<SingleProduct />} />
-				</Routes>
-				<Footer />
-			</BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/shop" element={<Shop />} />
+				<Route path="/shop/:category" element={<CategoryPage />} />
+				<Route path="/shop/:category/:slug" element={<SingleProduct />} />
+			</Routes>
+			<Footer />
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import App from './App';
 
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
+);
