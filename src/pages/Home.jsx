b4ee410219bf6d9e94d22054a0f4e51a7d7f381b
olawanlejoel <info4tobest@gmail.com>
2022-10-05 15:18:46 +0100

    fixed latest products section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import Hero from './../components/Hero';
 import Categories from './../components/Categories';
 
-import nike1 from './../assets/images/nike-1.jpg';
-import nike2 from './../assets/images/nike-2.jpg';
-import nike3 from './../assets/images/nike-3.jpg';
+import { request } from 'graphql-request';
+
+const API_KEY = process.env.REACT_APP_HYGRAPH_API_KEY;
+const API_URL = `https://api-us-east-1.graphcms.com/v2/${API_KEY}/master`;
 
 const Home = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchSneakers = async () => {
+			const { shoes } = await request(
+				API_URL,
+				`
+         { 
+            shoes(last: 3) {
+					id
+					name
+					price
+					slug
+					image {
+					  url
+					}
+					category {
+						slug
+					 }
+				 }
+         }
+       `
+			);
+
+			setProducts(shoes);
+		};
+		fetchSneakers();
+	}, []);
+
 	return (
 		<div>
 			<div className="container">
@@ -14,38 +47,28 @@ const Home = () => {
 			<div className="container new-arrivals-container">
 				<h2 className="title">New Arrivals</h2>
 				<div className="products">
-					<div className="product">
-						<img src={nike1} className="product-img" alt="" />
-						<div className="product-content">
-							<div className="flex-row">
-								<h3>Nike Air Max</h3>
-								<p className="price">$500</p>
+					{products.map((product) => (
+						<div key={product.id} className="product">
+							<img src={product.image.url} className="product-img" alt="" />
+							<div className="product-content">
+								<div className="flex-row">
+									<Link to={`${product.category.slug}/${product.slug}`}>
+										<h3>{product.name})</h3>
+									</Link>
+									<p className="price">${product.price}</p>
+								</div>
+								<button
+									className="snipcart-add-item btn"
+									data-item-id={product.id}
+									data-item-price={product.price}
+									data-item-image={product.image.url}
+									data-item-name={product.name}
+								>
+									Add to Cart
+								</button>
 							</div>
-							<button className="btn">Add to Cart</button>
 						</div>
-					</div>
-
-					<div className="product">
-						<img src={nike2} className="product-img" alt="" />
-						<div className="product-content">
-							<div className="flex-row">
-								<h3>Nike Air Max</h3>
-								<p className="price">$500</p>
-							</div>
-							<button className="btn">Add to Cart</button>
-						</div>
-					</div>
-
-					<div className="product">
-						<img src={nike3} className="product-img" alt="" />
-						<div className="product-content">
-							<div className="flex-row">
-								<h3>Nike Air Max</h3>
-								<p className="price">$500</p>
-							</div>
-							<button className="btn">Add to Cart</button>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 			<div className="container">
